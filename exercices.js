d1304adaa86b9f63d3df137e532429cce532943f
Karmadibsa <momper.axel.99@gmail.com>
2025-02-09 12:58:26 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -3,3 +3,9 @@ const nom_boutique = "Boutique de Boisson offrant des bienfaits";
 var nbPotionSoin = 10;
 var prixPotionSoin = 5;
 var ouvertureBoutique = false;
+
+if (ouvertureBoutique == true) {
+    console.log("Bienvenue dans la boutique "+ nom_boutique + " Aventurier ! 🎉");
+} else if (ouvertureBoutique == false) {
+    console.log("La boutique " + nom_boutique+ " est fermée, revenez plus tard Aventurier ! 😴`");
+}
